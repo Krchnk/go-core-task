41ba00a891d50274f53634ff23daa71081c89c11
Krchnk <a4mesh@gmail.com>
2025-01-27 23:36:34 +0300

    add task 1

--- 1/main1_test.go
+++ 1/main1_test.go
@@ -1,1 +1,76 @@
 package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"reflect"
+	"testing"
+)
+
+func TestPrintType(t *testing.T) {
+	tests := []struct {
+		input    interface{}
+		expected string
+	}{
+		{42, "int"},
+		{3.14, "float64"},
+		{"Golang", "string"},
+		{true, "bool"},
+		{complex64(1 + 2i), "complex64"},
+	}
+
+	for _, test := range tests {
+		result := printType(test.input)
+		if result != test.expected {
+			t.Errorf("printType(%v) = %v, ожидалось %v", test.input, result, test.expected)
+		}
+	}
+}
+
+func TestToString(t *testing.T) {
+	tests := []struct {
+		input    interface{}
+		expected string
+	}{
+		{42, "42"},
+		{3.14, "3.14"},
+		{"Golang", "Golang"},
+		{true, "true"},
+		{complex64(1 + 2i), "(1+2i)"},
+	}
+
+	for _, test := range tests {
+		result := toString(test.input)
+		if result != test.expected {
+			t.Errorf("toString(%v) = %v, ожидалось %v", test.input, result, test.expected)
+		}
+	}
+}
+
+func TestConcatenateVariables(t *testing.T) {
+	result := concatenateVariables(42, 3.14, "Golang", true, complex64(1+2i))
+	expected := "42 3.14 Golang true (1+2i) "
+	if result != expected {
+		t.Errorf("concatenateVariables() = %v, ожидалось %v", result, expected)
+	}
+}
+
+func TestStringToRunes(t *testing.T) {
+	input := "Golang"
+	expected := []rune{'G', 'o', 'l', 'a', 'n', 'g'}
+	result := stringToRunes(input)
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("stringToRunes(%v) = %v, ожидалось %v", input, result, expected)
+	}
+}
+
+func TestHashRunes(t *testing.T) {
+	runes := []rune{'G', 'o', 'l', 'a', 'n', 'g'}
+	salt := "go-2024"
+	expected := sha256.Sum256([]byte("Golgo-2024ang"))
+	expectedHash := hex.EncodeToString(expected[:])
+	result := hashRunes(runes, salt)
+	if result != expectedHash {
+		t.Errorf("hashRunes() = %v, ожидалось %v", result, expectedHash)
+	}
+}
--- 1/main_1.go
+++ 1/main_1.go
@@ -1,1 +1,84 @@
 package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+func printType(v interface{}) string {
+	return reflect.TypeOf(v).String()
+}
+
+func toString(v interface{}) string {
+	switch v := v.(type) {
+	case int:
+		return strconv.Itoa(v)
+	case float64:
+		return strconv.FormatFloat(v, 'f', -1, 64)
+	case string:
+		return v
+	case bool:
+		return strconv.FormatBool(v)
+	case complex64:
+		return fmt.Sprintf("%v", v)
+	default:
+		return ""
+	}
+}
+
+func concatenateVariables(vars ...interface{}) string {
+	var result strings.Builder
+	for _, v := range vars {
+		result.WriteString(toString(v))
+		result.WriteString(" ")
+	}
+	return result.String()
+}
+
+func stringToRunes(s string) []rune {
+	return []rune(s)
+}
+
+func hashRunes(runes []rune, salt string) string {
+	str := string(runes)
+	mid := len(str) / 2
+	saltedStr := str[:mid] + salt + str[mid:]
+
+	hash := sha256.Sum256([]byte(saltedStr))
+
+	return hex.EncodeToString(hash[:])
+}
+
+func main() {
+
+	var numDecimal int = 42
+	var numOctal int = 052
+	var numHexadecimal int = 0x2A
+	var pi float64 = 3.14
+	var name string = "Golang"
+	var isActive bool = true
+	var complexNum complex64 = 1 + 2i
+
+	fmt.Println("Типы переменных:")
+	fmt.Println("numDecimal:", printType(numDecimal))
+	fmt.Println("numOctal:", printType(numOctal))
+	fmt.Println("numHexadecimal:", printType(numHexadecimal))
+	fmt.Println("pi:", printType(pi))
+	fmt.Println("name:", printType(name))
+	fmt.Println("isActive:", printType(isActive))
+	fmt.Println("complexNum:", printType(complexNum))
+
+	combinedString := concatenateVariables(numDecimal, numOctal, numHexadecimal, pi, name, isActive, complexNum)
+	fmt.Println("\nОбъединенная строка:", combinedString)
+
+	runes := stringToRunes(combinedString)
+	fmt.Println("\nСрез рун:", runes)
+
+	salt := "go-2024"
+	hashedResult := hashRunes(runes, salt)
+	fmt.Println("\nХэшированный результат:", hashedResult)
+}
